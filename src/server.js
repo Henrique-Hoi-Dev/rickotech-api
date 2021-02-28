fa7ebf1421e8208a8237a680ff801d407541af2d
Henrique Hoinacki <riqueah@gmail.com>
2021-02-28 18:00:05 -0300

    dockerfile

--- src/server.js
+++ src/server.js
@@ -1,7 +1,4 @@
 import app from './app';
 
-var port = process.env.PORT || 3333;
-
-app.listen(port, function () {
-  console.log('Server running at http://127.0.0.1:%s', port);
-});
+console.log("server running",3000)
+app.listen(3000);
