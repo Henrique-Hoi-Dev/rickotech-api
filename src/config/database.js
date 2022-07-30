933cdd8545aebd10f4057d030d266e901e33a22a
Henrique Hoinacki <62766753+Henrique-Hoi-Dev@users.noreply.github.com>
2022-07-30 17:35:44 -0300

    .

--- src/config/database.js
+++ src/config/database.js
@@ -2,13 +2,17 @@ require('dotenv').config();
 
 module.exports = {
   dialect: 'postgres',
-  url: process.env.DATABASE_URL,
-  dialectOptions: { 
-    ssl: { 
-      require: true, 
-      rejectUnauthorized: false 
-    } 
-  },
+  // url: process.env.DATABASE_URL,
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,  
+  // dialectOptions: { 
+  //   ssl: { 
+  //     require: true, 
+  //     rejectUnauthorized: false 
+  //   } 
+  // },
   define: {
     timestamps: true,
     underscored: true,
