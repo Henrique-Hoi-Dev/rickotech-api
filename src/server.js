512a1998c2ab9e71a08a64d6debb979a9ecb3c0f
Henrique Hoinacki <62766753+Henrique-Hoi-Dev@users.noreply.github.com>
2022-07-30 15:51:27 -0300

    .

--- src/server.js
+++ src/server.js
@@ -1,8 +1,7 @@
 import app from './app';
-require = require("esm")(module);
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 app.listen(process.env.PORT || 3333);
-console.log("server running", process.env.PORT || 3333)
+console.log("server running!!!")
