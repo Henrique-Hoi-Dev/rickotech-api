4c4d7745c7d999285132e711efc96c016986eb56
Henrique Hoinacki <62766753+Henrique-Hoi-Dev@users.noreply.github.com>
2022-07-30 12:48:55 -0300

    database

--- src/config/database.js
+++ src/config/database.js
@@ -7,6 +7,12 @@ module.exports = {
   // username: "uvsgatgknbjnsb",
   // password: "b53b1a6a840eea327cb8060a62f9dce12f46d7fa566a94ef1b42eb2495e14d46",
   // database: "daldkdnf8tud4d",
+  dialectOptions: { 
+    ssl: { 
+      require: true, 
+      rejectUnauthorized: false 
+    } 
+  },
   define: {
     timestamps: true,
     underscored: true,
