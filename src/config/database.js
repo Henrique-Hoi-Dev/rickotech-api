95b637d162622e9249d2645a5e85482f9c0267d6
Henrique Hoinacki <62766753+Henrique-Hoi-Dev@users.noreply.github.com>
2022-07-30 13:02:12 -0300

    start

--- src/config/database.js
+++ src/config/database.js
@@ -3,10 +3,6 @@ require('dotenv/config');
 module.exports = {
   dialect: 'postgres',
   url: process.env.DATABASE_URL,
-  // host: "ec2-52-204-157-26.compute-1.amazonaws.com",
-  // username: "uvsgatgknbjnsb",
-  // password: "b53b1a6a840eea327cb8060a62f9dce12f46d7fa566a94ef1b42eb2495e14d46",
-  // database: "daldkdnf8tud4d",
   dialectOptions: { 
     ssl: { 
       require: true, 
