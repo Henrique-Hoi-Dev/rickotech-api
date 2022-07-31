7124943781579d430923e449a480053e0782bca4
Henrique Hoinacki <62766753+Henrique-Hoi-Dev@users.noreply.github.com>
2022-07-30 22:33:00 -0300

    .

--- src/config/database.js
+++ src/config/database.js
@@ -1,21 +1,27 @@
 require('dotenv/config');
 
 module.exports = {
-  dialect: 'postgres',
-  url: process.env.DATABASE_URL,
-  // host: process.env.POST_HOST,
-  // username: process.env.POST_USER,
-  // password: process.env.POST_PASS,
-  // database: process.env.POST_NAME,
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
-    }
+  production: {
+    url: process.env.DATABASE_URL,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    },
   },
-  define: {
-    timestamps: true,
-    underscored: true,
-    underscoredAll: true,
+  development: {
+    dialect: 'postgres',
+    host: process.env.POST_HOST,
+    username: process.env.POST_USER,
+    password: process.env.POST_PASS,
+    database: process.env.POST_NAME,
+
+    define: {
+      timestamps: true,
+      underscored: true,
+      underscoredAll: true,
+    },
   },
 };
--- src/database/index.js
+++ src/database/index.js
@@ -6,8 +6,9 @@ import Adress from '../app/models/Adress';
 import Order from '../app/models/Order';
 import FinancialBox from '../app/models/FinancialBox';
 import Service from '../app/models/Service';
+require('dotenv/config');
 
-import dataBaseConfig from '../config/database'
+// import dataBaseConfig from '../config/database'
 
 const models = [ 
   User, 
@@ -25,7 +26,26 @@ class Database {
   }
 
   init() {
-    this.connetion = new Sequelize(dataBaseConfig);
+    this.connetion  = new Sequelize(process.env.DATABASE_URL, {
+          dialectOptions: {
+            ssl: {
+              require: true,
+              rejectUnauthorized: false
+            }
+          }
+        }
+      );
+
+      this.connetion 
+        .authenticate()
+        .then(() => {
+          console.log('Connection has been established successfully.');
+        })
+        .catch(err => {
+          console.error('Unable to connect to the database:', err);
+        });
+
+    // this.connetion = new Sequelize(dataBaseConfig.production);
 
     models
       .map((model) => model.init(this.connetion))
