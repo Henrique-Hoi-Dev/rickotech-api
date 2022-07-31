7e14aa708f1aefd766028bd17aebbc9300986c86
Henrique Hoinacki <62766753+Henrique-Hoi-Dev@users.noreply.github.com>
2022-07-31 00:34:27 -0300

    .

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ import Service from './app/models/Service';
 
 // import './database';
 // console.log(process.env.DATABASE_URL)
+
 const models = [ 
   User, 
   Adress, 
@@ -23,6 +24,8 @@ const models = [
   FinancialBox, 
   Service
 ];
+
+const { DATABASE_URL } = process.env;
 
 class App {
   constructor() {
