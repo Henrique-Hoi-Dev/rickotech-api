7b82d2da55916e3fadde136ee9f0823b11cd3832
Henrique Hoinacki <62766753+Henrique-Hoi-Dev@users.noreply.github.com>
2022-08-01 12:33:43 -0300

    .

--- src/database/sequelize.js
+++ src/database/sequelize.js
@@ -2,7 +2,7 @@ const { Sequelize } = require("sequelize");
 
 import dataBaseConfig from '../config/database'
 
-const sequelize = new Sequelize(dataBaseConfig.production);
+const sequelize = new Sequelize(dataBaseConfig.development);
 
 //check connection (optional)
 sequelize
@@ -10,4 +10,4 @@ sequelize
   .then(() => console.log("Connection has been established successfully."))
   .catch((err) => console.error("Unable to connect to the database:", err));
 
-module.exports = sequelize;
+export default sequelize;
