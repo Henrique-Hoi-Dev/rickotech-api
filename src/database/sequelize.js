bb928278254e9a0ca9cb3457e439a5a2425ad50a
Henrique Hoinacki <62766753+Henrique-Hoi-Dev@users.noreply.github.com>
2022-08-01 14:47:24 -0300

    .

--- src/database/sequelize.js
+++ src/database/sequelize.js
@@ -11,12 +11,12 @@ import Service from '../app/models/Service';
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
-  // dialectOptions: { 
-  //   ssl: {
-  //     require: true,
-  //     rejectUnauthorized: false
-  //   },
-  // },
+  dialectOptions: { 
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    },
+  },
   define: {
     timestamps: true,
     underscored: true,
